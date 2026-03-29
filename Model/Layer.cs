3442609ac2d8a523498973c51dff6ec9eb3d37d4
MaksymMaryniuk <marinukmaks917@gmail.com>
2026-03-29 21:27:23 +0300

    Add project files.

---
+++ Model/Layer.cs
@@ -0,0 +1,63 @@
+﻿namespace Model;
+
+public class Layer
+{
+    public double[,] Weights { get; set; }
+    public double[] Biases { get; set; }
+    public double[,] Output { get; set; }
+    private Random rand = new Random();
+    public Layer(int num_inputs, int num_neurons)
+    {
+        Weights = new double[num_inputs, num_neurons];
+        Biases = new double[num_neurons];
+        Output = new double[num_inputs, num_neurons];
+        Matrix_filler(Weights);
+        Vector_filler(Biases);
+    }
+    public void Forward(double[,] X)
+    {
+        Output = Matrix_Multiplier(X, Weights, Biases);
+    }
+
+
+    private void Matrix_filler(double[,] value)
+    {
+
+        for (int i = 0; i < value.GetLength(0); i++)
+        {
+            for (int j = 0; j < value.GetLength(1); j++)
+            {
+                value[i, j] = (rand.NextDouble() * 2 - 1) * 0.1;
+            }
+        }
+    }
+    private void Vector_filler(double[] value)
+    {
+        for (int i = 0; i < value.Length; i++)
+        {
+            value[i] = rand.NextDouble() * 2 - 1;
+        }
+    }
+
+    private double[,] Matrix_Multiplier(double[,] matrix1, double[,] matrix2, double[] bias)
+    {
+        int output_rows = matrix1.GetLength(0);
+        int output_cols = matrix2.GetLength(1);
+        double[,] output = new double[output_rows, output_cols];
+
+        for (int rows = 0; rows < output_rows; rows++)
+        {
+            for (int cols = 0; cols < output_cols; cols++)
+            {
+                double sum = 0;
+                for (int common = 0; common < matrix1.GetLength(1); common++)
+                {
+                    sum += matrix1[rows, common] * matrix2[common, cols];
+                }
+                output[rows, cols] = sum + bias[cols];
+            }
+        }
+        return output;
+    }
+}
+
---
+++ Program/Program.cs
@@ -0,0 +1,92 @@
+﻿using ScottPlot;
+using Model;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int rows = 2;
+        int cols = 4;
+        double min = 4.0;
+        double max = 7.0;
+        double[,] matrix = new double[rows, cols];
+
+        Random rand = new Random();
+
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                double val = min + (rand.NextDouble() * (max - min));
+                matrix[i, j] = Math.Round(val, 2);
+            }
+        }
+        Console.WriteLine("------Basic Inputs (X)------");
+        PrintMatrix(matrix);
+        Console.WriteLine("----------------------------");
+
+        Model.Layer layer1 = new Model.Layer(4, 5);
+        Model.Layer layer2 = new Model.Layer(5, 1);
+
+        Console.WriteLine("------Weights------");
+        PrintMatrix(layer1.Weights);
+        Console.WriteLine("----------------------------");
+
+        Console.WriteLine("------Biases------");
+        for (int i = 0; i < layer1.Biases.Length; i++)
+        {
+            Console.Write(Math.Round(layer1.Biases[i], 2) + "\t");
+        }
+        Console.WriteLine("\n----------------------------");
+
+        layer1.Forward(matrix);
+        Console.WriteLine("------After forward------");
+        PrintMatrix(layer1.Output);
+        Console.WriteLine("----------------------------");
+        layer2.Forward(layer1.Output);
+        Console.WriteLine("------After forward 2------");
+        PrintMatrix(layer2.Output);
+        Console.WriteLine("----------------------------");
+        void PrintMatrix(double[,] matrix)
+        {
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(Math.Round(matrix[i, j], 2) + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        int pointsPerClass = 100;
+        int numClasses = 3;
+        var (X, y) = DataGenerator.CreateData(pointsPerClass, numClasses);
+        double[] xs = new double[pointsPerClass];
+        double[] ys = new double[pointsPerClass];
+
+
+        var plt = new ScottPlot.Plot();
+
+        plt.Title("Spiral Data (C# + ScottPlot)");
+
+        for (int c = 0; c < numClasses; c++)
+        {
+            for (int i = 0; i < pointsPerClass; i++)
+            {
+                int idx = c * pointsPerClass + i;
+                xs[i] = X[idx, 0];
+                ys[i] = X[idx, 1];
+            }
+            var scatter = plt.Add.Scatter(xs, ys);
+            scatter.LineWidth = 0; 
+            scatter.MarkerSize = 5;
+            Console.WriteLine(System.IO.Path.GetFullPath($"spiral_plot{c}.png"));
+            plt.SavePng($"spiral_plot{c}.png", 600, 600);
+        }
+
+        plt.ShowLegend();
+
+    }
+}
